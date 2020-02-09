bd6a777bd93f0bf82fd0fc045605121d9103c76e
Daniel Pati <50881345+watertown29@users.noreply.github.com>
2020-02-08 20:14:55 -0500

    Add model.php

---
+++ model.php
@@ -0,0 +1,26 @@
+<?php
+include("includes/init.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <title><?php if (isset($title)) { echo $title . " - "; } ?> INFO 2300</title>
+
+    <link rel="stylesheet" type="text/css" href="styles/site.css" media="all" />
+  </head>
+  <body>
+
+
+    <main>
+      <img class="hero-bg" src="/images/your-model.png" alt="">
+      <div class="button-invis"><a href="model.php">Click On this</a></div>
+
+    </main>
+
+
+  </body>
+
+</html>
