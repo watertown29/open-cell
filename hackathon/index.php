b6109a63d87edf014b6a3a794f9e2ab3d7623f85
Daniel Pati <50881345+watertown29@users.noreply.github.com>
2020-02-08 19:32:58 -0500

    Initial Commit

---
+++ hackathon/index.php
@@ -0,0 +1,25 @@
+<?php
+include("includes/init.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <title><?php if (isset($title)) { echo $title . " - "; } ?> INFO 2300</title>
+
+    <link rel="stylesheet" type="text/css" href="styles/site.css" media="all" />
+  </head>
+  <body>
+
+
+    <main>
+      <img class="hero-bg" src="/images/main.png" alt="">
+      <div class="image-block"></div>
+    </main>
+
+
+  </body>
+
+</html>
